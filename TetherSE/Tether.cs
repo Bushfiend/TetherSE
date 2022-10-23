96aa7b355fef207830ce5301b2c7707c15c790b1
TheCrunchy <crunchycamsta@gmail.com>
2022-10-23 13:09:25 +0100

    Add kinda cheaty grinder tether, if holding a grinder it moves all items to grid cargo that are ingot, component or ore

--- TetherSE/Tether.cs
+++ TetherSE/Tether.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Sandbox.Game;
 using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
 using Sandbox.Game.World;
 using Sandbox.ModAPI;
 using VRage.Game.Entity;
 using VRage.Game.ModAPI;
 using Sandbox.Game.Entities.Inventory;
+using Sandbox.Game.Weapons;
+using VRage.Game;
 using VRageMath;
 
 
@@ -27,38 +31,67 @@ namespace TetherSE
             var localPlayer = MySession.Static.LocalCharacter;
             IMyUtilities utils = MyAPIGateway.Utilities;
 
-            if (localPlayer.BuildPlanner.Count == 0)
-            {
-                return;
-            }
-
             if (ticks < 50)
             {
                 ticks++;
                 return;
             }
+
             ticks = 0;
 
-            if (GetTargetedBlock.selectedBlock != null)
+
+            if (GetTargetedBlock.selectedBlock == null)
             {
-                
-                if (Vector3D.Distance(localPlayer.PositionComp.GetPosition(), GetTargetedBlock.selectedBlock.GetPosition()) < Patches.maxUseDistance)
+                return;
+            }
+
+            if (Vector3D.Distance(localPlayer.PositionComp.GetPosition(),
+                    GetTargetedBlock.selectedBlock.GetPosition()) > Patches.maxUseDistance)
+            {
+                utils.ShowMessage("Tether Broke!",
+                    $"You Moved More Than {Patches.maxUseDistance}m from tethered block.");
+                GetTargetedBlock.selectedBlock = null;
+                GetTargetedBlock.selectedObject = null;
+                return;
+            }
+
+            var equippedTool = MySession.Static.LocalCharacter.HandItemDefinition.Id.SubtypeName;
+            if (equippedTool.Contains("Welder", StringComparison.OrdinalIgnoreCase))
+            {
+                if (localPlayer.BuildPlanner.Count == 0)
                 {
-                    ticks = 0;
-                    Patches.UseObjectPatch(Patches.maxUseDistance);
-                    Reflections.Withdraw.Invoke(null, new object[] { (MyEntity)GetTargetedBlock.selectedObject.Owner, localPlayer.GetInventory(0), null });
-                    Patches.UseObjectPatch(5f);
                     return;
-
-                } else
-                {
-
-                    utils.ShowMessage("Tether Broke!", $"You Moved More Than {Patches.maxUseDistance}m from tethered block.");
-                    GetTargetedBlock.selectedBlock = null;
-                    GetTargetedBlock.selectedObject = null;
-                    return; 
                 }
-            }     
+                DoWelder(localPlayer);
+                return;
+            }
+            if (equippedTool.Contains("Grinder", StringComparison.OrdinalIgnoreCase))
+            {
+                DoGrinder();
+                return;
+            }
+        }
+
+        public static void DoWelder(MyCharacter localPlayer)
+        {
+            Patches.UseObjectPatch(Patches.maxUseDistance);
+            Reflections.Withdraw.Invoke(null, new object[] { (MyEntity)GetTargetedBlock.selectedObject.Owner, localPlayer.GetInventory(0), null });
+            Patches.UseObjectPatch(5f);
+            return;
+        }
+
+        public static void DoGrinder()
+        {
+            var inventory = (MyInventory)GetTargetedBlock.selectedBlock.GetInventory();
+            foreach (var objectId in MySession.Static.LocalCharacter.GetInventory().GetItems())
+            {
+                if (!objectId.Content.GetObjectId().ToString().ToLower().Contains("ore") &&
+                    !objectId.Content.GetObjectId().ToString().ToLower().Contains("ingot") &&
+                    !objectId.Content.GetObjectId().ToString().ToLower().Contains("component")) continue;
+                MyConstants.DEFAULT_INTERACTIVE_DISTANCE = 10000;
+                MyInventory.TransferByPlanner(MySession.Static.LocalCharacter.GetInventory(), inventory, objectId.Content.GetObjectId(), MyItemFlags.None, objectId.Amount);
+                MyConstants.DEFAULT_INTERACTIVE_DISTANCE = 10;
+            }
         }
 
         public static int ticks = 0;
